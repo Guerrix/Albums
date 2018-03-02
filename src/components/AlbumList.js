40aff713e76d8ae7887eb765c7bb4fe444b39770
Jesus Guerra <jguerrix@gmail.com>
2018-03-01 17:22:31 -0700

    Do initial request to get albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>Album List!!!</Text>
