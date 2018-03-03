12ed52a7aacdd5593049cd57c26b2153eb6b7956
Jesus Guerra <jguerrix@gmail.com>
2018-03-03 07:38:16 -0700

    Create reducer boilerplate

--- TechStack/src/App.js
+++ TechStack/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 const App = () => (
-  <Provider store={createStore}>
+  <Provider store={createStore(reducers)}>
     <View />
   </Provider>
 );
