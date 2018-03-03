513f437c3c979c16aacb938b5d5261762fa07d21
Jesus Guerra <jguerrix@gmail.com>
2018-03-02 19:44:53 -0700

    Enable logout

--- Auth/src/App.js
+++ Auth/src/App.js
@@ -10,9 +10,13 @@ import {
   FIREBASE_MESSAGING_SENDER_ID
 } from 'react-native-dotenv';
 import LoginForm from './components/LoginForm';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: null };
+  }
   componentWillMount() {
     firebase.initializeApp({
       apiKey: FIREBASE_APIKEY,
@@ -22,13 +26,40 @@ class App extends Component {
       storageBucket: FIREBASE_STORAGE_BUCKET,
       messagingSenderId: FIREBASE_MESSAGING_SENDER_ID
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View flexDirection="row">
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+
+      default:
+        return (
+          <View style={{ alignSelf: 'center' }}>
+            <Spinner />
+          </View>
+        );
+    }
+  }
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
