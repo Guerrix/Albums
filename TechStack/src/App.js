dd1b50e250a65207cd9ab452679fb0e6c1ddc346
Jesus Guerra <jguerrix@gmail.com>
2018-03-03 10:05:46 -0700

    Display Library List

--- TechStack/src/App.js
+++ TechStack/src/App.js
@@ -3,10 +3,15 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import LibralyList from './components/LibralyList';
+import { Header } from './components/common';
 
 const App = () => (
   <Provider store={createStore(reducers)}>
-    <View />
+    <View style={{ flex: 1 }}>
+      <Header headerText="Tech Stack" />
+      <LibralyList />
+    </View>
   </Provider>
 );
 
