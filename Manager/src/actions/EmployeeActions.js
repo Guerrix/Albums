c540b80712d62400bd57bfa3f843060beedd2e1d
Jesus Guerra <jguerrix@gmail.com>
2018-03-10 06:23:45 -0700

    Add Create Employee stack

--- Manager/src/actions/EmployeeActions.js
+++ Manager/src/actions/EmployeeActions.js
@@ -1,8 +1,24 @@
-import { EMPLOYEE_UPDATE } from '../actions/types';
+import { Actions } from 'react-native-router-flux';
+import firebase from 'firebase';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from '../actions/types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  const { currentUser } = firebase.auth();
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.pop();
+      });
   };
 };
